1212d0a4268919020624eea1de9c7f5d60091831
Pratim Ghosh <pratim.ghosh@outlook.com>
2019-11-09 00:33:28 +0530

    added news api integration

--- newsloader/routes/news.js
+++ newsloader/routes/news.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var API_KEY = '';
+var API_KEY = '38b178162f2e497cb564c6a6dafc6c9a';
+var newsAPI = require('newsapi');
+var news = new newsAPI(API_KEY);
+
+var fs = require('fs');
+var dateFormat = require('dateformat');
 
 /* GET news listing. */
 router.get('/', function(req, res, next) {
@@ -11,8 +16,33 @@ router.get('/', function(req, res, next) {
 
 /* search news listing. */
 router.get('/search', function(req, res, next) {
-  console.log(req);
-  res.send({count: 1, location: ''});
+  console.log(req.query.keyword);
+  var searchResult = {keyword: req.query.keyword};
+  var timestamp = dateFormat(new Date(), 'yyyymmddHHMMssL');
+  news.v2.everything(
+    {q: req.query.keyword, pageSize: 100, sortBy: 'relevancy'},
+    (err, resp) => {
+      if (err) {
+        searchResult['count'] = 0;
+        res.send(searchResult);
+      } else {
+        console.log('writing to file');
+        var filePath =
+          'C:\\Users\\Pratim\\Desktop\\dump\\' +
+          timestamp +
+          '-' +
+          req.query.keyword +
+          '.json';
+        fs.writeFile(filePath, JSON.stringify(resp), () => {
+          console.log('written to file');
+          searchResult['count'] = resp.articles.length;
+          searchResult['data'] = resp;
+          searchResult['file'] = filePath;
+          res.send(searchResult);
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
