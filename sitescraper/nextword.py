340e36df9668c75ffad4319ae758f905a1dfd97a
Pratim Ghosh <pratim.ghosh@outlook.com>
2019-11-09 23:59:32 +0530

    added option to pick word from left

--- sitescraper/nextword.py
+++ sitescraper/nextword.py
@@ -11,17 +11,33 @@ def getwordgroup(sentence, searchword, maxright=0, expectstring=False):
     if searchword in words:
         indexofword = words.index(searchword)
         print("Found {" + searchword + "} at position " + str(indexofword + 1))
-        if (indexofword + maxright) >= len(words):
-            print("specified right limit exceed length, can use additional " +
-                  str(len(words) - words.index(searchword) - 1) + " word(s)")
-            searchresult = [words[ind]
-                            for ind in list(range(indexofword, len(words)))]
+        if maxright > 0:
+            if (indexofword + maxright) >= len(words):
+                print("specified right limit exceed length, can use additional " +
+                      str(len(words) - words.index(searchword) - 1) + " word(s)")
+                searchresult = [words[ind]
+                                for ind in list(range(indexofword, len(words)))]
+            else:
+                print("fetching " + str(maxright) +
+                      " word(s) right of position " + str(indexofword+1))
+                searchresult = [
+                    words[ind]
+                    for ind in list(
+                        range(indexofword, indexofword + maxright + 1))]
         else:
-            print("fetching " + str(maxright) +
-                  " word(s) from position " + str(indexofword+1))
-            searchresult = [
-                words[ind]
-                for ind in list(range(indexofword, indexofword + maxright))]
+            if (indexofword + maxright) < 0:
+                print(
+                    "specified left limit is below zero, can use additional " +
+                    str(words.index(searchword)) + " word(s)")
+                searchresult = [words[ind]
+                                for ind in list(range(0, indexofword + 1))]
+            else:
+                print("fetching " + str(maxright) +
+                      " word(s) before position " + str(indexofword + 1))
+                searchresult = [
+                    words[ind]
+                    for ind in list(
+                        range(indexofword + maxright, indexofword + 1))]
     else:
         print("No matching word found")
     if expectstring:
@@ -39,11 +55,18 @@ def getwordgroupby(sentence, searchstring, maxright=0, expectstring=True):
         searchresult = getwordgroup(
             sentence, searchstring, maxright, True)
     else:
-        print("search string has multiple word, using last word to search")
-        searchresult = getwordgroup(
-            sentence, searchwords[-1], maxright, True)
-        # print(list2string(searchwords[:-1]))
-        searchresult = list2string(searchwords[:-1]) + " " + searchresult
+        print("search string has multiple word")
+        if maxright > 0:
+            print("using last word to search")
+            searchresult = getwordgroup(
+                sentence, searchwords[-1], maxright, True)
+            # print(list2string(searchwords[:-1]))
+            searchresult = list2string(searchwords[:-1]) + " " + searchresult
+        else:
+            print("using first word to search")
+            searchresult = getwordgroup(
+                sentence, searchwords[0], maxright, True)
+            searchresult = searchresult + " " + list2string(searchwords[1:])
     if not expectstring:
         searchresult = searchresult.split(" ")
     print(searchresult)
@@ -57,6 +80,16 @@ def getwordgroupby(sentence, searchstring, maxright=0, expectstring=True):
 # getwordgroup(text, "is", 2)
 # getwordgroup(text, "text", 2)
 
-getwordgroupby(text, "is a", 2)
-getwordgroupby(text, "is a", 20)
-getwordgroupby(text, "text", 2)
+# getwordgroupby(text, "is a", 2)
+# getwordgroupby(text, "is a", 20)
+# getwordgroupby(text, "text", 2)
+
+# getwordgroup(text, "this", -2)
+# getwordgroup(text, "text", -2)
+# getwordgroup(text, "sample", -20)
+
+# getwordgroupby(text, "a", 2)
+# getwordgroupby(text, "a", -2)
+
+# getwordgroupby(text, "is a", 2)
+# getwordgroupby(text, "a sample", -2)
